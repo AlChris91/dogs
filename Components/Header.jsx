34454f1d09906a089a12f7fcaff72b818b5dc867
Álan <xalanchristian@outlook.com>
2025-07-28 13:40:38 -0300

    Adicionado a estilização e rotas para o componente Header

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import Dogs from "../src/Assets/dogs.svg?react";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs className="logo" />
+        </Link>
+        <Link className={styles.logo} to="/login">
+          Login / Criar
+        </Link>
       </nav>
     </header>
   );
